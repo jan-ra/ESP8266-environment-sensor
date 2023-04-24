bbd03c767a4c5ec34424b25fe9fcbd90665fde83
Jan Ramdohr <jan.ramdohr@outlook.com>
2023-04-24 21:42:58 +0200

    add noise sampling

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,7 @@
 #define PURGETIME 300000
 #define SAMPLERATE 60000
 #define LED D0
+#define AUDIO_SENSOR_PIN A0
 
 // VARIABLES
 String timeServer = "https://timeapi.io/api/Time/current/zone?timeZone=Europe/Amsterdam";
@@ -28,6 +29,9 @@ unsigned int seconds = 0;
 unsigned int channel = 1;
 unsigned int ledstatus = true;
 unsigned int lastLEDon = true;
+const int sampleWindow = 50;
+unsigned int sample;
+int db;
 int clients_known_count_old;
 BH1750 lightMeter;
 
@@ -44,9 +48,11 @@ void setup()
 {
   Serial.begin(115200);
   Wire.begin(D2, D1);
+
   delay(10);
   StaticJsonDocument<500> doc;
   pinMode(LED, OUTPUT);
+  pinMode(AUDIO_SENSOR_PIN, INPUT);
   lightMeter.begin();
   Serial.println('\n');
   Serial.println("Initializing SD cards");
@@ -156,12 +162,33 @@ void loop()
     }
     if (millis() % SAMPLERATE == 0)
     {
+      unsigned long startMillis = millis(); // Start of sample window
+      float peakToPeak = 0;                 // peak-to-peak level
+      unsigned int signalMax = 0;           // minimum value
+      unsigned int signalMin = 1024;        // maximum value
+      while (millis() - startMillis < sampleWindow)
+      {
+        sample = analogRead(AUDIO_SENSOR_PIN);
+        if (sample < 1024) // toss out spurious readings
+        {
+          if (sample > signalMax)
+          {
+            signalMax = sample; // save just the max levels
+          }
+          else if (sample < signalMin)
+          {
+            signalMin = sample; // save just the min levels
+          }
+        }
+      }
+      peakToPeak = signalMax - signalMin; // max - min = peak-peak amplitude
+      db = map(peakToPeak, 20, 900, 49.5, 90);
       float lux = lightMeter.readLightLevel();
       Serial.print("Light: ");
       Serial.print(lux);
       Serial.println(" lx");
-      writeData(outputDevices() + "," + String(lux));
-      log("wrote data -> " + outputDevices() + "," + String(lux));
+      writeData(outputDevices() + "," + String(lux) + "," + String(db));
+      log("wrote data -> " + outputDevices() + "," + String(lux) + "," + String(db));
     }
     if (millis() % 5000 == 0)
     {
@@ -260,3 +287,4 @@ String outputDevices()
   String out = String(strong) + "," + String(medium) + "," + String(weak);
   return out;
 }
+´´
